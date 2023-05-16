6cb2091cb910809974b64eba8ddbb7a839494137
VicMB243 <61756454+VicMB243@users.noreply.github.com>
2023-05-16 19:23:41 +0300

    Update db.js

--- db.js
+++ db.js
@@ -22,7 +22,7 @@ const mysql = require('mysql');
 //     });
 
     var mysqlConnection = mysql.createConnection({
-        host: '127.0.0.1:306',
+        host: '127.0.0.1:3306',
         user: 'admin',
         password: 'p@55Admin',
         database: 'shopiadatabase',
