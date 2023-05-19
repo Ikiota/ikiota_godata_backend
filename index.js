756cb2c759bf1d4d082182dd802a041b961030bc
VicMB243 <61756454+VicMB243@users.noreply.github.com>
2023-05-19 12:36:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ const socketio = require('socket.io')(http);
    
    
 
-app.use('/uploads', Express.static(__dirname +'/uploads'));
+//app.use('/uploads', Express.static(__dirname +'/uploads'));
    
     app.get("/api/", (req, res) => {
        res.send("Hello from shopia!We are live now!!!");
