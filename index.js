e8ba41aa133f4ebb8fd9f4bd45061bbc6e620271
VicMB243 <61756454+VicMB243@users.noreply.github.com>
2023-05-19 12:37:32 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const Express = require('express');
 const cors = require('cors');
 const mongoose = require("mongoose");
 
-const uploadsRouter = require("./uploads")
 const adminsRouter = require("./routes/admins")
 const buyersRouter  = require("./routes/buyers")
 const driversRouter  = require("./routes/drivers")
@@ -66,7 +65,7 @@ const socketio = require('socket.io')(http);
    
    
 
-//app.use('/uploads', Express.static(__dirname +'/uploads'));
+app.use('/uploads', Express.static(__dirname +'/uploads'));
    
     app.get("/api/", (req, res) => {
        res.send("Hello from shopia!We are live now!!!");
